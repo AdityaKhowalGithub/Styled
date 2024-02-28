5918a8b0b12370631a7fbd9d0e1b1e3a310cddb7
Aditya Khowal <adityakh2003@outlook.com>
2024-02-27 17:35:07 -0800

    fixde camera

--- Capstone/app/(tabs)/add.tsx
+++ Capstone/app/(tabs)/add.tsx
@@ -1,121 +1,22 @@
-// import React, { useState, useEffect, useRef } from 'react';
-// import { Alert, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-// import { Camera } from 'expo-camera';
-// import { useIsFocused, useNavigation, useRoute } from '@react-navigation/native';
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-// // This would be your AddPieceScreen component
-// const AddPieceScreen = () => {
-//   const [hasPermission, setHasPermission] = useState(null);
-//   const [hasUnsavedChanges, setHasUnsavedChanges] = useState(false);
-//   const isFocused = useIsFocused(); // We use this to know when the tab is focused
-//   const navigation = useNavigation();
-//   const route = useRoute();
-//   const cameraRef = useRef(null);
-
-//   // Ask for camera permission
-//   useEffect(() => {
-//     (async () => {
-//       const { status } = await Camera.requestCameraPermissionsAsync();
-//       setHasPermission(status === 'granted');
-//     })();
-//   }, []);
-
-//   useEffect(() => {
-//     if (isFocused && hasUnsavedChanges) {
-//       const discardChanges = () => {
-//         setHasUnsavedChanges(false);
-//         // Here you would reset the state of your component as necessary
-//       };
-
-//       Alert.alert(
-//         "Discard changes?",
-//         "Are you sure you want to discard this piece without saving?",
-//         [
-//           { text: "Don't leave", style: 'cancel', onPress: () => {} },
-//           { text: 'Discard', style: 'destructive', onPress: discardChanges },
-//         ]
-//       );
-//     }
-//   }, [isFocused, hasUnsavedChanges]);
-
-//   if (hasPermission === null) {
-//     return <View />;
-//   }
-//   if (hasPermission === false) {
-//     return <Text>No access to camera</Text>;
-//   }
-
-//   // Function to handle taking a picture
-//   const handleTakePicture = async () => {
-//     if (cameraRef.current) {
-//       const photo = await cameraRef.current.takePictureAsync();
-//       console.log(photo);
-//       setHasUnsavedChanges(true); // We now have an unsaved photo
-//       // Process the photo as necessary
-//     }
-//   };
-
-//   return (
-//     <View style={{ flex: 1 }}>
-//       <Camera ref={cameraRef} style={styles.camera}>
-//         <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
-//           <Text style={styles.text}>Take Picture</Text>
-//         </TouchableOpacity>
-//       </Camera>
-//     </View>
-//   );
-// };
-
-// // Tab navigator setup
-// const Tab = createBottomTabNavigator();
-
-// // Main component that includes the tab navigator
-// const MainComponent = () => {
-//   return (
-//     <Tab.Navigator>
-//       {/* Other tabs */}
-//       <Tab.Screen name="Add" component={AddPieceScreen} />
-//       {/* Other tabs */}
-//     </Tab.Navigator>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   camera: {
-//     flex: 1,
-//     justifyContent: 'flex-end',
-//     alignItems: 'center',
-//   },
-//   button: {
-//     flex: 0.1,
-//     alignSelf: 'flex-end',
-//     alignItems: 'center',
-//     backgroundColor: 'white',
-//     marginBottom: 20,
-//     padding: 10,
-//     borderRadius: 5,
-//   },
-//   text: {
-//     fontSize: 18,
-//     color: 'black',
-//   },
-// });
-
-// export default MainComponent;
 import React, { useState, useEffect, useRef } from 'react';
 import { Alert, View, Text, TouchableOpacity, Image, TextInput, StyleSheet, ScrollView } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useIsFocused, useNavigation, useRoute } from '@react-navigation/native';
 
+
+
 const AddPieceScreen = () => {
+  const navigation = useNavigation();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({ headerShown: false });
+  }, [navigation]);
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState('');
   const [size, setSize] = useState('');
   const [brand, setBrand] = useState('');
   const [photo, setPhoto] = useState(null);
   const cameraRef = useRef(null);
-  const navigation = useNavigation();
 
   // Request camera permissions
   useEffect(() => {
@@ -190,52 +91,71 @@ const AddPieceScreen = () => {
           </View>
         </ScrollView>
       ) : (
-        // <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef}>
-        //   <View style={styles.buttonContainer}>
-        //     <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
-        //       <Text style={styles.text}> Take Photo </Text>
-        //     </TouchableOpacity>
-        //   </View>
-        // </Camera>
-        <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef}>
-  <View style={styles.overlay}>
-    <View style={styles.overlayBox} />
-    <Text style={styles.instructionsText}>Make sure your piece is within this box!</Text>
-  </View>
-  <View style={styles.buttonContainer}>
-    <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
-      <Text style={styles.text}>Take Photo</Text>
-    </TouchableOpacity>
-  </View>
-</Camera>
 
-    
+        <View style={styles.container}>
+          <View style={styles.cameraWrapper}>
+            <Camera style={styles.camera} type={Camera.Constants.Type.back} ref={cameraRef}>
+              <View style={styles.overlay}>
+                {/* <View style={styles.overlayBox} /> */}
+
+                <View style={[styles.corner, styles.topLeftCorner, { top: 0, left: 0 }]} />
+                <View style={[styles.corner, styles.topRightCorner, { top: 0, right: 0 }]} />
+                <View style={[styles.corner, styles.bottomLeftCorner, { bottom: 0, left: 0 }]} />
+                <View style={[styles.corner, styles.bottomRightCorner, { bottom: 0, right: 0 }]} />
+                <Text style={styles.instructionsText}>Make sure your piece is within this box!</Text>
+
+                <Text style={styles.instructionsText}>Make sure your piece is within this box!</Text>
+              </View>
+
+            </Camera>
+          </View>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={handleTakePicture}>
+
+            </TouchableOpacity>
+          </View>
+        </View>
+
+
       )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: 'black', // Usually camera screen has a black background
+    backgroundColor: 'white', // Set the background color of the whole screen
+  },
+
+  cameraWrapper: {
+    width: '80%', // Adjust the width to what you want
+    alignSelf: 'center', // This will center the cameraWrapper
+    marginTop: 100, // Add some margin at the top
+    aspectRatio: 0.5625, // Keep the aspect ratio of the cameraWrapper
+    borderWidth: 1, // Add a border to see the box clearly
+    borderColor: 'black', // Set the border color
   },
   camera: {
-    flex: 1,
-    justifyContent: 'space-between', // This will allow you to position elements at the top and bottom of the screen
+    flex: 1, // Camera should fill the entire space of its container
   },
   buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center', // This centers the button on the screen
-    marginBottom: 20, // Assuming the button is at the bottom
+    position: 'absolute', // This positions your button container absolutely within its parent
+    left: 0, // Aligns the container to the left
+    right: 0, // Aligns the container to the right
+    bottom: 20, // Positions the container 20 pixels from the bottom of the screen
+    justifyContent: 'center', // Centers the button horizontally
+    alignItems: 'center', // Centers the button vertically (useful if the container has a height)
   },
   button: {
     width: 70, // A typical size for a camera button
     height: 70, // A typical size for a camera button
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff', // Camera button is usually white
+    backgroundColor: '#4A4A4A', // Camera button is usually white
     borderRadius: 35, // This will make it round
+    // Add shadows or other styling as needed
   },
   text: {
     fontSize: 18,
@@ -252,7 +172,7 @@ const styles = StyleSheet.create({
   },
   overlayBox: {
     width: '90%',
-    height: '80%',
+    height: '90%',
     borderWidth: 2,
     borderColor: 'white',
     backgroundColor: 'transparent',
@@ -261,31 +181,12 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: '50%', // Center vertically
     alignSelf: 'center', // Center horizontally
-    color: 'white',
+    color: '#000000',
+    opacity: 0.5, // Make the text semi-transparent
     textAlign: 'center',
     paddingHorizontal: 20, // Make sure the text doesn't touch the sides
   },
-  // container: {
-  //   flex: 1,
-  // },
-  // camera: {
-  //   flex: 1,
-  // },
-  // buttonContainer: {
-  //   flex: 1,
-  //   backgroundColor: 'transparent',
-  //   flexDirection: 'row',
-  //   margin: 20,
-  // },
-  // button: {
-  //   flex: 0.1,
-  //   alignSelf: 'flex-end',
-  //   alignItems: 'center',
-  // },
-  // text: {
-  //   fontSize: 18,
-  //   color: 'white',
-  // },
+
   scrollView: {
     flex: 1,
     backgroundColor: '#fff',
@@ -333,6 +234,31 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color: '#fff',
   },
+
+
+  corner: {
+    position: 'absolute',
+    width: 30, // Width of the corner
+    height: 30, // Height of the corner
+    borderColor: 'white', // Corner color
+  },
+  topLeftCorner: {
+    borderTopWidth: 5, // Thickness of the border
+    borderLeftWidth: 5, // Thickness of the border
+  },
+  topRightCorner: {
+    borderTopWidth: 5,
+    borderRightWidth: 5,
+  },
+  bottomLeftCorner: {
+    borderBottomWidth: 5,
+    borderLeftWidth: 5,
+  },
+  bottomRightCorner: {
+    borderBottomWidth: 5,
+    borderRightWidth: 5,
+  },
+
 });
 
 export default AddPieceScreen;
