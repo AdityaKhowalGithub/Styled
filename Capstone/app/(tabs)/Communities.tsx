ea61af48052ca52bb96681e000f074bd6dee8fb2
jychung9 <72417992+jychung9@users.noreply.github.com>
2024-03-07 23:40:07 -0800

    update

--- Capstone/app/(tabs)/Communities.tsx
+++ Capstone/app/(tabs)/Communities.tsx
@@ -1,39 +1,54 @@
 import Colors from '@/constants/Colors';
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TouchableHighlight, Alert, Button, Modal } from 'react-native';
+import { View, Text, StyleSheet, Image, ScrollView, TouchableHighlight, TextInput, Alert, Modal, Pressable } from 'react-native';
+import UploadImage from '@/components/UploadImage'
 
 const CommunitiesScreen = ({}) => {
 	
   // State to store post data
   const [communityData, setCommunityData] = useState({
 		communityName: '',
-    communityPicUri: '',
     memberCount: '',
     communityBio: '',
+    communityLead: '',
   });
 
-	var [ isPress, setIsPress ] = React.useState(false);
-	var touchProps = {
-    activeOpacity: 1,
-    underlayColor: 'blue',                               // <-- "backgroundColor" will be always overwritten by "underlayColor"
-    style: isPress ? styles.btnPress : styles.btnNormal, // <-- but you can still apply other style changes
-    onHideUnderlay: () => setIsPress(false),
-    onShowUnderlay: () => setIsPress(true),
-    onPress: () => console.log('HELLO'),                 // <-- "onPress" is apparently required
-  };
+  const [postData, setPostData] = useState({
+    user: '',
+    communityName: '',
+    caption: '',
+  });
 
+  const IMAGES: {[key: string]: any} = {
+    "seattlevintage": require("@/assets/images/seattlevintagepfp.png"),
+    "christiller": require("@/assets/images/christillerpfp.png"),
+    "post": require("@/assets/images/postpic.png")
+  }
+
+	const [communityVisible, setCommunityVisible] = useState(false);
+  const [postVisible, setPostVisible] = useState(false);
+
+  const [text, onChangeText] = React.useState('');
+	
   useEffect(() => {
     // Dummy data for the feed
     const dummyData1 = {
         communityName: 'Seattle Vintage Collect',
-				communityPicUri: require("@/assets/images/seattlevintagepfp.png"),
         memberCount: '1,031 members',
         communityBio: 'Find new fashion inspiration and share your unique style with others.',
+        communityLead: 'jenniferlovespink'
+      };
+
+    const dummyPostData = {
+      user: 'Chris Tiller',
+      communityName: 'Y2K mix',
+      caption: 'Suggestions on shoes that match?',
     };
     
     // Simulating a fetch request with a timeout
     setTimeout(() => {
       setCommunityData(dummyData1);
+      setPostData(dummyPostData);
     }, 1000);
   });
 
@@ -45,27 +60,211 @@ const CommunitiesScreen = ({}) => {
 		<ScrollView style={styles.container}>
 			
 			<View style={styles.headerContainer}>
-				<Text style={styles.header}>Communities for you</Text>
+				<Text style={styles.categories}>Communities for you</Text>
 			</View>
       <View style={styles.communityContainer}>
-			    <Image
+        <View style={styles.communityHead}>
+          <Image
           style={styles.profileImage}
-          source={{ uri: communityData.communityPicUri }}
-        />
-        <Text style={styles.postText}>
-					<Text style={styles.name}>{communityData.communityName}</Text>
-				</Text>
-				<Button
-        title="Join"
-        onPress={() => Alert.alert('Join button pressed')}
-      	/>
-				<Text style={styles.postText}>
-					<Text style={styles.name}>{communityData.communityBio}</Text>
-				</Text>
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Modal
+            animationType="slide"
+            transparent={false}
+            visible={communityVisible}
+            onRequestClose={() => {
+              setCommunityVisible(!communityVisible);
+            }}>
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+
+                
+                <ScrollView>
+                  <View style={styles.headerContainer}>
+                    <Text style={styles.header}>{communityData.communityName}</Text>
+                  </View>
+                  <View style={styles.spacer}></View>
+                  <View style={styles.communityHead}>
+                    <Image
+                      style={{                         
+                        width: 70,
+                        height: 70,
+                        borderRadius: 50,
+                        marginBottom: 10,
+                        marginRight: 10,
+                        alignContent: 'flex-start'
+                      }}
+                      source={ IMAGES["seattlevintage"] }
+                    />
+                    <Text style={styles.name}>Community Lead {"\n"}@{communityData.communityLead}</Text>
+                  </View>
+                    <Text style={styles.postText}>{communityData.communityBio}</Text>
+                    <Text style={styles.policy}>Community Policy</Text>
+                    
+                    <Pressable
+                      style={[styles.button, styles.buttonOpen]}
+                      onPress={() => setPostVisible(true)}>
+                      <Text style={styles.textStyle}>Add Post</Text>
+                    </Pressable>
+                              
+                  <View style={styles.postContainer}>
+                    <View style={styles.communityHead}>
+                      <Image
+                        style={styles.profileImage}
+                        source={IMAGES["christiller"]}
+                      />
+                      <Text style={styles.postText}>
+                        <Text style={styles.name}>{postData.user}</Text>
+                      </Text>
+                    </View>
+                      <Image
+                        style={styles.postImage}
+                        source={IMAGES["post"]}
+                      />
+                      <Text style={styles.postText}>
+                        {postData.caption}
+                      </Text>
+                  </View>
+                </ScrollView>
+
+                
+                <TouchableHighlight
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setCommunityVisible(!communityVisible)}>
+                  <Text style={styles.textStyle}>Close</Text>
+                </TouchableHighlight>
+              </View>
+            </View>
+          </Modal>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+			  
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>
+        
+				
       </View>
-      <Text style={styles.header}>Top in Seattle</Text>
-      <Text style={styles.header}>Top in USA</Text>
-      
+      <View style={styles.communityContainer}>
+        <View style={styles.communityHead}>
+          <Image
+          style={styles.profileImage}
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>	
+      </View>
+      <View style={styles.headerContainer}>
+				<Text style={styles.categories}>Top in Seattle</Text>
+			</View>
+      <View style={styles.communityContainer}>
+        <View style={styles.communityHead}>
+          <Image
+          style={styles.profileImage}
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>	
+      </View>
+      <View style={styles.communityContainer}>
+        <View style={styles.communityHead}>
+          <Image
+          style={styles.profileImage}
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>	
+      </View>
+      <View style={styles.headerContainer}>
+				<Text style={styles.categories}>Top in USA</Text>
+			</View>
+      <View style={styles.communityContainer}>
+        <View style={styles.communityHead}>
+          <Image
+          style={styles.profileImage}
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>	
+      </View>
+      <View style={styles.communityContainer}>
+        <View style={styles.communityHead}>
+          <Image
+          style={styles.profileImage}
+          source={ IMAGES["seattlevintage"] }
+          />
+          <Text style={styles.postText}>
+            <Text style={styles.name}>{communityData.communityName}</Text>
+          </Text>
+          <Pressable
+            style={[styles.joinButton, styles.buttonOpen]}
+            onPress={() => setCommunityVisible(true)}>
+            <Text style={styles.textStyle}>Join</Text>
+          </Pressable>
+        </View>
+        <Text style={{
+          alignContent: 'center'
+        }}>{communityData.memberCount}
+        </Text>
+        <Text style={styles.postText}>{communityData.communityBio}</Text>	
+      </View>
 
     </ScrollView>
   );
@@ -77,28 +276,32 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   headerContainer: {
-    padding: 20,
+    padding: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
 	header: {
-		fontSize: 48,
+		fontSize: 25,
 		fontWeight: 'bold',
 		color: '#333',
+    marginTop: 25,
 	},
   categories: {
-    fontSize: 60,
-    fontWeight: 'bold',
-    color: '#333',
-  },
+		fontSize: 25,
+		fontWeight: 'bold',
+		color: '#333',
+    margin: 10,
+	},
   profileImage: {
-    width: 10,
-    height: 10,
+    width: 50,
+    height: 50,
     borderRadius: 50,
     marginBottom: 10,
+    marginRight: 10,
+    alignContent: 'flex-start'
   },
   name: {
-    fontSize: 24,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#333',
   },
@@ -107,21 +310,33 @@ const styles = StyleSheet.create({
     color: 'grey',
   },
   communityContainer: {
-    alignItems: 'center',
+    flex: 1 ,
     padding: 20,
     borderBottomWidth: 1,
     borderBottomColor: '#ccc',
   },
+  postContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  communityHead: {
+    flexDirection: 'row',
+
+  },
 	postText: {
-		fontSize: 24,
+		fontSize: 12,
 		color: 'black',
 	},
 	postImage: {
-		width: 150,
-		height: 150,
-		borderRadius: 50,
+		width: 200,
+		height: 200,
+		borderRadius: 10,
 		marginBottom: 10,
 	},
+  postCaption: {
+    fontSize: 15,
+    color: 'black',
+  },
 	icon: {
 		width: 8,
 		height: 8,
@@ -139,6 +354,58 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     height: 30,
     width: 100,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'space-around',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 1,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 5,
+  },
+  joinButton: {
+    borderRadius: 5,
+    padding: 10,
+    elevation: 5,
+    marginLeft: 20,
+    width: 60,
+    height: 40,
+  },
+  buttonOpen: {
+    backgroundColor: '#A86154',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    textAlign: 'left',
+  },
+	input: {
+    height: 40,
+    margin: 2,
+    borderWidth: 1,
+    padding: 10,
+  },
+  policy: {
+    fontSize: 20,
+    color: '#A86154',
+    padding: 20,
+  },
+  spacer: {
+    paddingBottom: 20,
   }
 	
 });
